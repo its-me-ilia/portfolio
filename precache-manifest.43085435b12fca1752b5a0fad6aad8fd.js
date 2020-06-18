cd549ffd2ac0aa6703dce9287dcec9a26e19a129
its-me-ilia <its.me.ilia04@gmail.com>
2020-06-18 19:11:40 +0400

    Updates

---
+++ precache-manifest.43085435b12fca1752b5a0fad6aad8fd.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ef9ff9725ea980e6f69de9e27bed06",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f26c7e2d59e22aecc926",
+    "url": "/portfolio/static/css/main.2d9dc268.chunk.css"
+  },
+  {
+    "revision": "4b3c83d6d79b53b10b28",
+    "url": "/portfolio/static/js/2.309fb575.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.309fb575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26c7e2d59e22aecc926",
+    "url": "/portfolio/static/js/main.f7246158.chunk.js"
+  },
+  {
+    "revision": "52484cc7424f9cebacd9",
+    "url": "/portfolio/static/js/runtime-main.70bb1dcf.js"
+  },
+  {
+    "revision": "8ded9998151bb87f92b7999400e6794b",
+    "url": "/portfolio/static/media/arrow.8ded9998.svg"
+  },
+  {
+    "revision": "4982c65e5e00fd173d842007b6c1e81a",
+    "url": "/portfolio/static/media/contact.4982c65e.jpg"
+  },
+  {
+    "revision": "c8d54e217632b2ce9ca70411af1c14fb",
+    "url": "/portfolio/static/media/css-3.c8d54e21.svg"
+  },
+  {
+    "revision": "dd21833286b8a91e323443b6030b53bf",
+    "url": "/portfolio/static/media/email.dd218332.svg"
+  },
+  {
+    "revision": "e36d17c77ba531e88221737f39d410e7",
+    "url": "/portfolio/static/media/express.e36d17c7.svg"
+  },
+  {
+    "revision": "efea3ea059c7e9b3982aac265ecf5b0f",
+    "url": "/portfolio/static/media/fblogo.efea3ea0.svg"
+  },
+  {
+    "revision": "b3fe99f8b276442fd5dc5d06b55d6dae",
+    "url": "/portfolio/static/media/ghlogo.b3fe99f8.svg"
+  },
+  {
+    "revision": "ceada846e22b573648ccab533abfea2f",
+    "url": "/portfolio/static/media/gmaillogo.ceada846.svg"
+  },
+  {
+    "revision": "5aaf688bc4943f6a33b76f9fe3b1db48",
+    "url": "/portfolio/static/media/hirest.5aaf688b.png"
+  },
+  {
+    "revision": "a745037d8380b653a8dbb6a16d4c449c",
+    "url": "/portfolio/static/media/javascript.a745037d.svg"
+  },
+  {
+    "revision": "b75cd9732909332e76f814b12f171490",
+    "url": "/portfolio/static/media/linlogo.b75cd973.svg"
+  },
+  {
+    "revision": "a31f5a87497add709c136be89a7de24c",
+    "url": "/portfolio/static/media/mongodb.a31f5a87.svg"
+  },
+  {
+    "revision": "9eed603251f7d6d5f7f3673e1773c288",
+    "url": "/portfolio/static/media/nodejs.9eed6032.svg"
+  },
+  {
+    "revision": "bc68dea590e7bf0048f9af46d23edd0d",
+    "url": "/portfolio/static/media/nologo.bc68dea5.png"
+  },
+  {
+    "revision": "3e6d7d5aef60c6f633611d19142cb13e",
+    "url": "/portfolio/static/media/part.3e6d7d5a.svg"
+  },
+  {
+    "revision": "8e3ec836668ba5df4305077d32954561",
+    "url": "/portfolio/static/media/pencil.8e3ec836.svg"
+  },
+  {
+    "revision": "ec0deace6dff0aece26b5bc04efe1a57",
+    "url": "/portfolio/static/media/phone.ec0deace.svg"
+  },
+  {
+    "revision": "a5ce1beb979c6bc0f250ddb429414223",
+    "url": "/portfolio/static/media/pinktravel.a5ce1beb.png"
+  },
+  {
+    "revision": "351ea234a2640eb241f2aa2b054c9ec9",
+    "url": "/portfolio/static/media/portal.351ea234.png"
+  },
+  {
+    "revision": "67043764a7dfe3831eb20cab34ea00d3",
+    "url": "/portfolio/static/media/programmer.67043764.jpg"
+  },
+  {
+    "revision": "a108479b9e2b1216ce9f086faf966e22",
+    "url": "/portfolio/static/media/react.a108479b.svg"
+  },
+  {
+    "revision": "f4d0630a22e1b0e6e484c04b93b1691f",
+    "url": "/portfolio/static/media/redux.f4d0630a.svg"
+  },
+  {
+    "revision": "5b5ed364de5862599b74dedadd043673",
+    "url": "/portfolio/static/media/rocket.5b5ed364.svg"
+  },
+  {
+    "revision": "8ecc6712f8a73bf7be329883ce8456b8",
+    "url": "/portfolio/static/media/typescript-icon.8ecc6712.svg"
+  }
+]);
